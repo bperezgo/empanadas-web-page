f2a565d113dc34b8fd59270f198e3aa781d73aa4
Bryan Pérez <bryanper@Bryans-MacBook-Air.local>
2021-05-24 23:15:11 -0500

    feat: ProductDescription with styles

--- components/ProductDescription/index.tsx
+++ components/ProductDescription/index.tsx
@@ -1,5 +1,11 @@
 import styles from './styles.module.scss';
 
 export const ProductDescription = () => (
-  <div className={styles.ProductDescription}>ProductDescription</div>
+  <div className={styles.ProductDescription}>
+    <div className={styles.ProductDescription__title}>Title of Product</div>
+    <div className={styles.ProductDescription__description}>
+      Description waou of this amazing product
+    </div>
+    <div className={styles.ProductDescription__price}>$ 2.000</div>
+  </div>
 );
