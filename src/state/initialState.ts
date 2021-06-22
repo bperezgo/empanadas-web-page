a38c74f646a881907d7ac039d509fb65a4e51f30
Bryan Pérez <bryanper@Bryans-MacBook-Air.local>
2021-06-21 20:41:58 -0500

    fix: travis.yml file is corrected

---
+++ src/state/initialState.ts
@@ -0,0 +1,20 @@
+export const initialState = [
+  {
+    title: 'Some title',
+    description: 'Description waou of this amazing product',
+    imgUrl: '',
+    price: 2000,
+  },
+  {
+    title: 'Some title',
+    description: 'Description waou of this amazing product',
+    imgUrl: '',
+    price: 2000,
+  },
+  {
+    title: 'Some title',
+    description: 'Description waou of this amazing product',
+    imgUrl: '',
+    price: 2000,
+  },
+];
